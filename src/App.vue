0e25da172271eed2e108f5cf44cc776c03a374eb
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-05 23:45:55 +0300

    Code Execution

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
 	<div>
-  <a :href="href">{{text}}</a>
+    <h1>Квадрат числа {{ num }} = {{ num * num }}</h1>
+  </div>
+
+  <div>
+    <h1>Сумма чисел {{ num1}}, {{num2}}, {{num3}} = {{ num1 + num2 + num3 }}</h1>
   </div>
 </template>
 
 <script>
 export default {
 	data() {
-	return {
-		text: 'Vue3',
-		href: 'page.html',
-	}
-}
+	  return {
+		  num: 11,
+      num1: 27,
+		  num2: 9,
+		  num3: 2003,
+	  }
+  } 
 }
 
 </script>
