e28b6752eb87056fc3015ae6c914d20d55ea2690
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-06 23:35:51 +0300

    v-else

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
 
-  <button  @click="one">{{ visible_1 ? 'HIDE' : 'SHOW' }}</button>
-  <p v-if="visible_1" style="color: rgb(238, 87, 87);">Red</p>
-  <br>
-
-  <button  @click="two">{{ visible_2 ? 'HIDE' : 'SHOW' }}</button>
-  <p v-if="visible_2" style="color: rgb(83, 199, 91);">Green</p>
-  <br>
-
-  <button  @click="three" >{{ visible_3 ? 'HIDE' : 'SHOW' }}</button>
-  <p v-if="visible_3" style="color: rgb(83, 141, 199);">Blue</p>
+  <button  @click="one">PRESS ME</button>
+  <p v-if="isAdmin" style="color: rgb(83, 199, 91);">YES :)</p>
+  <p v-else style="color: rgb(238, 87, 87);"> NO :(</p>
 
   <div id='cmd_head' v-if="false">
   <p>Console</p>
@@ -26,22 +19,14 @@
 export default {
 	data() {
     return {
-      visible_1: true,
-      visible_2: true,
-      visible_3: true,
+      isAdmin: true,
 	  }
   },
 
   methods: {
     one: function() {
-      this.visible_1 = !this.visible_1;
+      this.isAdmin = !this.isAdmin;
     },
-    two: function() {
-      this.visible_2 = !this.visible_2;
-    },
-    three: function() {
-      this.visible_3 = !this.visible_3;
-    }
   }
 }
 
