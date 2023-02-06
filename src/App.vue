637048683d51d421e63ae76cec655b16e34bdc69
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-06 23:26:30 +0300

    Toggle 2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
 
-  <button  @click="one">ONE</button>
+  <button  @click="one">{{ visible_1 ? 'HIDE' : 'SHOW' }}</button>
   <p v-if="visible_1" style="color: rgb(238, 87, 87);">Red</p>
   <br>
 
-  <button  @click="two">TWO</button>
+  <button  @click="two">{{ visible_2 ? 'HIDE' : 'SHOW' }}</button>
   <p v-if="visible_2" style="color: rgb(83, 199, 91);">Green</p>
   <br>
-  
-  <button  @click="three" >THREE</button>
+
+  <button  @click="three" >{{ visible_3 ? 'HIDE' : 'SHOW' }}</button>
   <p v-if="visible_3" style="color: rgb(83, 141, 199);">Blue</p>
 
   <div id='cmd_head' v-if="false">
