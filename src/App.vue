bf4dd26f25bfd31abf69c96b66d6f631d4ab2456
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-13 22:52:37 +0300

    Reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
 
-  <button  @click="hide">PRESS ME</button>
+  <button  @click="push_num(nums)">PUSH</button>
+  <button  @click="del_last(nums)">DELETE LAST</button>
+  <button  @click="del_first(nums)">DELETE FIRST</button>
+  <button  @click="del_last2(nums)">DELETE LAST LAST</button>
+  <button  @click="sort_num(nums)">SORT</button>
+  <button  @click="sort_num_inv(nums)">SORT INV</button>
 
   <div id='cmd_head'>
   <p>Console</p>
@@ -9,7 +14,7 @@
   <div id='cmd'>
     <p>> Задание 1. Вывести только положительные числа.</p>
     <ul>
-      <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+      <li v-for="num in nums">{{ num }}</li>
     </ul>  
   </div>
 
@@ -19,27 +24,39 @@
 export default {
 	data() {
 	  return {
-		  products: [
-        {
-          id: 1,
-          name: 'Product_1',
-        },
-        {
-          id: 2,
-          name: 'Product_2',
-        },
-        {
-          id: 3,
-          name: 'Product_3',
-        },
-		  ],
+		  nums: [1, 2, 3],
 	  }
   },
 
   methods: {
-    hide(){
-      this.isAuth = !(this.isAuth);
-    }
+    push_num(arr){
+      arr.push(arr.length + 1);
+    },
+    del_first(arr){
+      arr.splice(0,1);
+    },
+    del_last(arr){
+      arr.splice(arr.length - 1);
+    },
+    del_last2(arr){
+      arr.splice(arr.length - 2, 1);
+    },
+    compare(a, b) {
+      if (a > b) return 1;
+      if (a == b) return 0;
+      if (a < b) return -1;
+    },
+    sort_num(arr){
+      arr.sort(this.compare);
+    },
+    compare_inv(a, b) {
+      if (a < b) return 1;
+      if (a == b) return 0;
+      if (a > b) return -1;
+    },
+    sort_num_inv(arr){
+      arr.sort(this.compare_inv);
+    },
   }
 }
 
