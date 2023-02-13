e37675bea2d9fca09a64b921b4919332f8528273
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-13 18:37:49 +0300

    Range

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,7 @@
 
   <div id='cmd'>
     <p>> Задание 1</p>
-    <ul>
-      <li v-for="elem in hrefs"><a :href="elem.href">{{elem.text}}</a></li>
-    </ul>
-    <p>> Задание 2</p>
-    <table>
-      <td>Название </td> 
-      <td>Цена </td>  
-      <td>Количество </td>
-      <tr v-for="product in products">
-        <td>{{ product.name }}</td>
-        <td>{{ product.price }}</td>
-        <td>{{ product.quantity }}</td>
-      </tr>
-    </table>
+    <p v-for="num in 30">Абзац номер {{ num }}</p>    
   </div>
 
 </template>
@@ -30,28 +17,7 @@
 export default {
 	data() {
 	  return {
-		  hrefs: [
-        {href: '1.html', text: 'text1'},
-        {href: '2.html', text: 'text2'},
-        {href: '3.html', text: 'text3'},
-		  ],
-      products: [
-        {
-          name: 'Product_2',
-          price: 100,
-          quantity: 5
-        },
-        {
-          name: 'Product_2',
-          price: 200,
-          quantity: 4
-        },
-        {
-          name: 'Product_3',
-          price: 300,
-          quantity: 3
-        },
-		  ]
+		  
 	  }
   },
 
