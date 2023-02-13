de6c9a1ef43620e802c2ed1585862580232c0507
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-13 18:34:16 +0300

    Enumeration

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,21 @@
   </div>
 
   <div id='cmd'>
-    <div v-for="elem in items">
-	    <p>> {{ elem }}</p>
-	    <p class="divider"></p>
-    </div>
-
-    <ul v-for="elem in items">
-      <li>{{ elem }}</li>
-      <li class="divider"></li>
+    <p>> Задание 1</p>
+    <ul>
+      <li v-for="elem in hrefs"><a :href="elem.href">{{elem.text}}</a></li>
     </ul>
+    <p>> Задание 2</p>
+    <table>
+      <td>Название </td> 
+      <td>Цена </td>  
+      <td>Количество </td>
+      <tr v-for="product in products">
+        <td>{{ product.name }}</td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.quantity }}</td>
+      </tr>
+    </table>
   </div>
 
 </template>
@@ -24,7 +30,28 @@
 export default {
 	data() {
 	  return {
-		  items: [1, 2, 3],
+		  hrefs: [
+        {href: '1.html', text: 'text1'},
+        {href: '2.html', text: 'text2'},
+        {href: '3.html', text: 'text3'},
+		  ],
+      products: [
+        {
+          name: 'Product_2',
+          price: 100,
+          quantity: 5
+        },
+        {
+          name: 'Product_2',
+          price: 200,
+          quantity: 4
+        },
+        {
+          name: 'Product_3',
+          price: 300,
+          quantity: 3
+        },
+		  ]
 	  }
   },
 
