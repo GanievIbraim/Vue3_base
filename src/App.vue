82d4c3ca5949bc8d1bf630d6586df781f559bea2
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-13 17:00:37 +0300

    Group

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
 
-  <button  @click="one">PRESS ME</button>
+  <button  @click="hide">PRESS ME</button>
 
   <div id='cmd_head'>
   <p>Console</p>
   </div>
 
   <div id='cmd'>
-    <p v-if="age <= 18">> Подросток</p>
-	  <p v-else-if=" 19 < age < 25">> Молодой человек</p>
-	  <p v-else-if="age >= 26">> Мужчина</p>
-	  <p v-else>error</p>
+    <template v-if="isAuth">
+		  <p>> Абзац номер 1</p>
+		  <p>> Paragraph number 2</p>
+		  <p>> la la</p>
+    </template>
   </div>
 
 </template>
@@ -19,11 +20,14 @@
 export default {
 	data() {
     return {
-      age: 25,
+      isAuth: true,
 	  }
   },
 
   methods: {
+    hide(){
+      this.isAuth = !(this.isAuth);
+    }
   }
 }
 
