b5cc332b4d982b7892a39ddc234d3ec25ed2711f
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-22 01:44:47 +0300

    Inline object

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
 
-  <button @click="hide">Hide</button>
-  <button @click="show">Show</button>
-  <button @click="toggle">Toggle</button>
+  <button>Press me</button>
 
   <div id='cmd_head'>
   <p>Console</p>
   </div>
   
   <div id='cmd'>
-    <p :class="obj">> Задание 39. Теперь этот абзац тоже виден.</p>
-    <p class="done">> Задание 39. Этот абзац видно, a первый нет.</p> 
+    <p :class="{active: true, valid: false}">> Задание 40. Теперь этот абзац тоже виден.</p>
   </div>
 
 </template>
@@ -19,35 +16,22 @@
 export default {
 	data() {
 	return {
-		obj: {
-			hidden: true,
-      done: true,
-		},
 	}
 },
 
   methods: {
-    hide: function() {
-		  this.obj.hidden = true;
-	  },
-    show: function() {
-      this.obj.hidden = false;
-    },
-    toggle: function() {
-      this.obj.hidden = !this.obj.hidden;
-    }
   }
 }
 
 </script>
 
 <style>
-  .done{
+  .active{
     font-family: Consolas;
     font-size: 1.5em;
     color: rgb(4, 194, 118);
   }
-  p.hidden {
-	  display: none;
+  .valid{
+	  text-decoration: underline;
 }
 </style>
