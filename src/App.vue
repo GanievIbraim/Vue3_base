d1422d005faf66f14a5e1604ffb810192c658b76
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-25 09:08:13 +0300

    Deleting objects from an array

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,21 @@
   
   <div id='cmd'>
     <p>> Нажмите на элемент списка для удаления</p>
-    <ul>
-      <li v-for="(item, index) in arr" :key="index" @click="removeItem(index)">
-			  {{ item }}
-		  </li>
-    </ul>
+    <table>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>salary</th>
+        <th>age</th>
+      </tr>
+      <tr v-for="user in users" :key="user.id">
+        <th> {{ user.id }} </th>
+        <th> {{ user.name }} </th>
+        <th> {{ user.salary }} </th>
+        <th> {{ user.age }} </th>
+        <th @click="removeItem(user.id)"> del </th>
+      </tr>
+    </table>
   </div>
 
 </template>
@@ -23,13 +33,34 @@ let date = new Date();
 export default {
 	data() {
     return {
-      arr: ['lala', 'haha', 'hola', 'Mavile'],
+      users: [
+        {
+          id: 1,
+          name: 'name1',
+          salary: 100,
+          age: 30,
+        },
+        {
+          id: 2,
+          name: 'name2',
+          salary: 200,
+          age: 40,
+        },
+        {
+          id: 3,
+          name: 'name3',
+          salary: 300,
+          age: 50,
+        },
+		  ],
     }
   },
 
   methods: {
-    removeItem: function(index) {
-		  this.arr.splice(index, 1);
+    removeItem: function(id) {
+		  this.users = this.users.filter((user) => {
+			  return user.id !== id;
+		  })
 	  }
   }
 }
@@ -40,9 +71,18 @@ export default {
   #cmd p, li, span{
     font-family: Consolas;
     font-size: 1.2em;
-    color: rgba(0, 199, 76, 0.709);
+    color: rgba(0, 199, 76);
   }
   textarea{
     color: black;
+  }
+  table{
+    border-collapse: collapse; 
+    
+  }
+  th{
+    border: 1px solid rgba(0, 199, 76);
+    color: rgba(0, 199, 76);
+    width: 60px;
   }
 </style>
